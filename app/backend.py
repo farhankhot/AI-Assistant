8c7723fad28b76d8770c96a779de2acc6fe30920
Farhan K <farhan.crazyeyes@gmail.com>
2023-02-23 16:21:52 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -441,7 +441,7 @@ def linkedin_login():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    chrome_options.binary_location = 'GOOGLE_CHROME_BIN'
     driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=chrome_options)
     
     driver.get(r.url)
