28900a00e6bb9df19f2c5421df4714b0143b4259
Farhan K <farhan.crazyeyes@gmail.com>
2023-02-24 16:39:20 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -470,7 +470,7 @@ def linkedin_login():
     submit_button = driver.find_element(By.CSS_SELECTOR, ".btn__primary--large")
     submit_button.click()
     
-    wait = WebDriverWait(driver, 1)  
+    wait = WebDriverWait(driver, timeout=29)  
     captcha_iframe = wait.until(EC.presence_of_element_located((By.ID, "captcha-internal")))
     print(captcha_iframe)
     
