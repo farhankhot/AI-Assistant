79fd648251809a4afae253fbbe8b61e55ec3e53c
Farhan K <farhan.crazyeyes@gmail.com>
2023-02-28 01:29:27 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -576,7 +576,7 @@ def linkedin_login():
                 print(audio_response_textbox.get_attribute('value'))
                 audio_submit_button = driver.find_element(By.ID, "audio_submit")
                 audio_submit_button.click()
-                time.sleep(10)
+                time.sleep(20)
                 print(driver.current_url)
                 
             except sr.UnknownValueError:
