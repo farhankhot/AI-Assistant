c935940b4d193ef3c4fe4746c45cbf151f15e606
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-04 16:58:42 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -33,6 +33,7 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('disable-dev-shm-usage')
+chrome_options.add_argument('window-size=1920x1480')
 
 prefs = {"download.default_directory": r"~/",
         "directory_upgrade": True}
