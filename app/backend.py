a8c54ce05cd0607284ad2560a4218a0d9a6c3b47
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-04 16:35:47 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -573,9 +573,10 @@ def linkedin_login():
         switch_to_audio_button.click()
         # print("after clicking audio button", driver.page_source)
             
-        # time.sleep(5) 
+        time.sleep(5) 
+        download_audio_button = driver.find_element(By.ID, "audio_download")
         
-        download_audio_button = wait.until(EC.element_to_be_clickable((By.ID, "audio_download")))
+        # download_audio_button = wait.until(EC.element_to_be_clickable((By.ID, "audio_download")))
 
         download_audio_button.click()
         
