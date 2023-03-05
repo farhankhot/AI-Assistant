1a47f2548dabb7325cddcc59358af94c86dda170
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-05 18:30:23 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -565,7 +565,7 @@ def linkedin_login():
         # print("html after verify_button clicked", driver.page_source)
         
         # third iframe contains button to download wav file
-        switch_to_audio_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="fc_meta_audio_btn"]/span')))
+        switch_to_audio_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="fc_meta_audio_btn"]')))
         
         print(switch_to_audio_button.get_attribute("style"))
         switch_to_audio_button.click()
