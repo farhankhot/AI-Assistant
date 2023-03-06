ca0f2f60e0d729986be580a9b5fbecbc525d2b78
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-05 21:55:15 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -532,22 +532,7 @@ def linkedin_login():
     if (url.startswith("https://www.linkedin.com/checkpoint")):
         
         # print(driver.page_source)
-    
-        payload = {
-            'csrfToken': driver.find_element(By.NAME, 'csrfToken').get_attribute('value'),
-            'pageInstance': driver.find_element(By.NAME, 'pageInstance').get_attribute('value'),
-            # 'resendUrl': driver.find_element(By.NAME, 'resendUrl').get_attribute('value'),
-            'challengeId': driver.find_element(By.NAME, 'challengeId').get_attribute('value'),
-            'language': 'en-US',
-            'displayTime': driver.find_element(By.NAME, 'displayTime').get_attribute('value'),
-            'challengeSource': driver.find_element(By.NAME, 'challengeSource').get_attribute('value'),
-            'requestSubmissionId': driver.find_element(By.NAME, 'requestSubmissionId').get_attribute('value'),
-            'challengeType': driver.find_element(By.NAME, 'challengeType').get_attribute('value'),
-            'challengeData': driver.find_element(By.NAME, 'challengeData').get_attribute('value'),
-            'challengeDetails': driver.find_element(By.NAME, 'challengeDetails').get_attribute('value'),
-            'failureRedirectUri': driver.find_element(By.NAME, 'failureRedirectUri').get_attribute('value')
-        }   
-    
+        
         wait = WebDriverWait(driver, timeout=29)  
         captcha_iframe = wait.until(EC.presence_of_element_located((By.TAG_NAME, "iframe")))
         driver.switch_to.frame(captcha_iframe)
