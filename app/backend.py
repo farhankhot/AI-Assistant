ac1f2a8dccf69a9cb62b55ffc9591d2451b377ed
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-06 16:57:46 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -665,6 +665,7 @@ def linkedin_login():
                 # geo_urn = re.search("\d+", geo_urn).group()
                 # print(geo_urn)
                 # return jsonify(success=True, message="success")
+                data = q.enqueue(GetProfile, api, title, 103644278, False)
                 
             except sr.UnknownValueError:
                 print('Unable to transcribe audio')
