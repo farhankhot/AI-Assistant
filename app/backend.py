6bcc6b07846cbc50051d9f35476255b1a9b618d0
Farhan K <farhan.crazyeyes@gmail.com>
2023-03-06 21:12:45 -0500

    Update backend.py

--- app/backend.py
+++ app/backend.py
@@ -31,7 +31,7 @@ session = requests.Session()
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
 chrome_options.add_argument('disable-dev-shm-usage')
 chrome_options.add_argument('window-size=1920x1480')
 
